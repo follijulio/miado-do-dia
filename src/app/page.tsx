8c02eb687381c96eb9fdfcd1ba68344048712651
follii <follijulio@gmail.com>
2025-12-25 06:13:32 -0300

    feat: restructure Page component and add NavBar with HandDrawCard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+import { HandDrawCard } from "@/components/ui/hand-drawn-card";
+
 export default function Page() {
   return (
-    <div>
-      <div>
-        <h1>Bem vindo</h1>
-      </div>
+    <div className="h-screen w-screen p-4 text-white">
+      <NavBar />
+      <main className="pt-4 flex"></main>
     </div>
   );
 }
+
+const NavBar = () => {
+  return (
+    <HandDrawCard scribble={3} curvature={1}>
+      <nav className="h-20 w-full flex items-center p-5">
+        <p className="text-white">[] NAV BAR - TODO</p>
+      </nav>
+    </HandDrawCard>
+  );
+};
