8b9c30c5b2468b2a5cf1e6f3b92a77af724a2e99
follii <follijulio@gmail.com>
2026-01-01 22:31:29 -0300

    feat: wrap main content in MainLayout component for consistent layout structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,22 @@
 "use client";
 
 import Card from "@/components/ui/cards";
+import MainLayout from "@/components/ui/root/main-layout";
 import chartData from "@/mock/chart_data";
 import { todos } from "@/mock/todos";
 import Chart from "./../components/ui/charts/index";
 
 export default function Page() {
   return (
-    <main className="flex flex-row gap-4 overflow-hidden min-h-0  h-full">
-      <div className="h-96 w-1/2">
-        <Chart.BarActive data={chartData} className="h-full w-full p-6" />
-      </div>
-      <div className="h-96 w-1/2">
-        <Card.TodoList className="h-full w-full" todos={todos} />
-      </div>
-    </main>
+    <MainLayout>
+      <main className="flex flex-row gap-4 overflow-hidden min-h-0 h-full">
+        <div className="h-96 w-1/2">
+          <Chart.BarActive data={chartData} className="h-full w-full p-6" />
+        </div>
+        <div className="h-96 w-1/2">
+          <Card.TodoList className="h-full w-full" todos={todos} />
+        </div>
+      </main>
+    </MainLayout>
   );
 }
