2b0997dafd94d5dfae1450e1c925e732f47380d7
follijulio <follijulio@gmail.com>
2026-01-23 14:20:55 -0300

    feat: add bcryptjs dependency and update user API routes

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,9 +1,5 @@
-import { CreateUserController } from '@/adapters/api/controllers/create-user-crontroller';
 import { NextRequest } from 'next/server';
 
 export async function POST(request: NextRequest) {
   
-  const controller = new CreateUserController();
-
-  return controller.handle(await request.json());
 }
